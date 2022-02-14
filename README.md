851061a8b8d483610496d4efd5b23855ecc0ec9e
YanShkl <yshkleinik@gmail.com>
2022-02-14 18:05:18 +0300

    First Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Text-to-Morse
+
+This is a simple project what converts English text to morse code.
--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
+import sys
+from files import welcome, MORSE_CODE_DICT
 
-text = input('Input text You want to translate to morse code: ').upper()
+print(f'\n{welcome}') # Showing welcome ASCII
 
-enc_word = []
-for letter in text:
-    if letter != ' ':
-        print(type(letter))
-        enc_word.append(f'{MORSE_CODE_DICT[letter]}')
-print(enc_word)
+is_working=True
+
+while is_working:
+
+    text = input('\nInput text You want to translate to morse code or "EXIT" to exit: ').upper()
+
+    enc_word = []
+
+    def text_to_morse():
+
+        for letter in text:
+
+            if text == 'EXIT':
+                global is_working
+
+                print('You have exited the program.')
+                is_working = False
+                break
+
+            if letter != ' ':
+                try:
+                    enc_word.append(f'{MORSE_CODE_DICT[letter]}')
+
+                except KeyError:
+                    print(f'You have entered not allowed symbol/letter: {sys.exc_info()[1]}. \n'
+                          f'Please enter only English letters next time, without special symbols.')
+                    break
+    text_to_morse()
+
+    print(f" Your morse code is: {' '.join(enc_word)}")
